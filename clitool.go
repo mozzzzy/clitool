3c1e35ae6aa7d2d0650534e454660ff980c5eb93
mozzzzy <kurt.goedel.1109@gmail.com>
2020-01-09 00:35:03 +0900

    refactor: remove useless comment

--- clitool.go
+++ clitool.go
@@ -44,39 +44,14 @@ func New() *Terminal {
 	return terminal
 }
 
-func (terminal *Terminal) Spinner(messageStr string, finished *bool) {
-	spnr := spinner.New(messageStr)
-	go spnr.Spin(terminal.x, terminal.y, finished)
-	terminal.x, terminal.y = common.GoNextLine(terminal.x, terminal.y)
-}
-
 func (terminal *Terminal) List(question string, choiceStrs []string) (answerStr string) {
 	lst := list.New(choiceStrs)
 	answerStr, terminal.x, terminal.y = lst.Inquire(question, terminal.x, terminal.y)
 	return answerStr
 }
 
-/*
-func main() {
-	Init()
-
-	terminal := Terminal{
-		x: 0,
-		y: 0,
-	}
-
-	lst := list.New([]string{"go", "javascript", "c++"})
-	_, terminal.x, terminal.y = lst.Inquire("What is your favorite language?", terminal.x, terminal.y)
-
-	fin := new(bool)
-	*fin = false
-	spnr := spinner.New("Please wait for 5 seconds")
-	go spnr.Spin(terminal.x, terminal.y, fin)
-
-	time.Sleep(5 * time.Second)
-	*fin = true
-
-	time.Sleep(2 * time.Second)
-	Close()
+func (terminal *Terminal) Spinner(messageStr string, finished *bool) {
+	spnr := spinner.New(messageStr)
+	go spnr.Spin(terminal.x, terminal.y, finished)
+	terminal.x, terminal.y = common.GoNextLine(terminal.x, terminal.y)
 }
-*/
