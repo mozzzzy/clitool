6f76ebf270e0ce12f4755c165b38533620686110
mozzzzy <kurt.goedel.1109@gmail.com>
2020-01-09 19:52:55 +0900

    refactor: remove useless field from question

--- question/question.go
+++ question/question.go
@@ -15,7 +15,6 @@ import (
  */
 
 type Question struct {
-	PrefixStr   string
 	QuestionStr string
 	AnswerStr   string
 }
@@ -44,8 +43,6 @@ var AnswerColorBg termbox.Attribute = termbox.ColorDefault
 
 func New(questionStr string) *Question {
 	question := new(Question)
-	question.PrefixStr = PrefixStr
-	question.QuestionStr = questionStr
 	return question
 }
 
